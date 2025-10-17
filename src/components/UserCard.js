67ce59ed87edbd03a86fe5d57e72095988f9a611
jeff <sddeh5981@gmsil.com>
2025-10-17 20:44:45 +0300

    d

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
-function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
-  
-  export default UserCard;
+function UserCard({ user }) {
+  return (
+    <article>
+      <h2>{user.name}</h2>
+      <Link to={`/profile/${user.id}`}>View profile</Link>
+    </article>
+  );
+}
+
+export default UserCard;
+k;
